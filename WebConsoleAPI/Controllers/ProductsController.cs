7460c464069263023c0ad7ce1351f7e65d511103
timonsk <regetadm@gmail.com>
2016-08-18 17:12:27 +0300

    Initial commit;

---
+++ WebConsoleAPI/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebConsoleAPI.Models;
+
+namespace WebConsoleAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private readonly Product[] products =
+        {
+            new Product {Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1},
+            new Product {Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
+            new Product {Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M}
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ WebConsoleAPI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace WebConsoleAPI
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var config = new HttpSelfHostConfiguration("http://localhost:3032");
+
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new {id = RouteParameter.Optional});
+            var appXmlType =
+                config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            using (var server = new HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
